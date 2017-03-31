9811c9fc27df33bde4e5d48e6184e467902b68cc
Carol217 <cpan3@stuy.edu>
2017-03-31 10:26:40 -0400

    Deque interface

---
+++ Deque.java
@@ -0,0 +1,23 @@
+
+public interface Deque<T>{
+
+    //adds a value to the front
+    public void addFirst ( T val );
+    
+    //adds a value to the end
+    public void addLast ( T val );
+
+    //removes a value from the front
+    public T removeFirst ();
+
+    //removes a value from the end
+    public T removeLast ();
+
+    //retrieves value at front
+    public T offerFirst ();
+
+    //retrieves value from end
+    public T offerLast ();
+
+
+}
