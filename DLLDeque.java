fd09c19981183bddc0b8b6a7cf8224426a9b2b18
Mark Shafran <Mark Shafran>
2017-04-01 11:05:03 -0400

    Added implementation for all methods in DLLDeque

--- DLLDeque.java
+++ DLLDeque.java
@@ -1,3 +1,8 @@
+// Team AllCAMerasOnDeque (Carol Pan, Adrian Boylan, Mark Shafran)
+// APCS2 pd05
+// Lab02--All Hands On Deque!
+// 2017-04-04
+
 public class DLLDeque<T> implements Deque<T>{
     
     private DLLNode<T> _first, _last;
@@ -10,46 +15,59 @@ public class DLLDeque<T> implements Deque<T>{
 
     public void addFirst (T val) {
 
-	//implementation here
-
+	if (isEmpty()) {
+	    _first.setCargo(val);
+	    _last = _first;
+	    _size++;
+	    return;
+	}
+	_first.setPrev(new DLLNode<T>(val, null, _first));
+	_first = getPrev();
+	_size++;
+	return;
     }
 
     public void addLast(T val) {
-
-	//implementation here
-
+	if (isEmpty()) {
+	    _last.setCargo(val);
+	    _first = _last;
+	    _size++;
+	    return;
+	}
+	_last.setNext(new DLLNode<T>(val, _last, null));
+	_last = _last.getNext();
+	_size++;
+	return;
     }
 
     public T removeFirst() {
-
-	//implementation here
-	return null;
-
+        if (isEmpty())
+	    {throw new IndexOutOfBoundsException("There is no element to remove");}
+	T temp = _first.getCargo();
+	_first = _first.getNext();
+	_size++;
+	return temp;
     }
 
     public T removeLast() {
-
-	//implementation here
-	return null;
-
+        if (isEmpty())
+	    {throw new IndexOutOfBoundsException("There is no element to remove");}
+	T temp = _first.getCargo();
+	_first = _first.getNext();
+	_size++;
+	return temp;
     }
 
     public T offerFirst() {
-
-	//implementation here
-	return null;
-
-    }
+        return _first.getCargo();
+}
 
     public T offerLast() {
-
-	//implementation here
-	return null;
-
+	return _last.getCargo()
     }
 
     public boolean isEmpty() {
-	return true;
+	return (_size==0);
     }
 
     public String toString() { 
