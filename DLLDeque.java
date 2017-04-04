d2a8361584dbc03de7e43a81ecfb1930ffcbdc9b
Mark Shafran <Mark Shafran>
2017-04-03 21:11:06 -0400

    made more indepth comments

--- DLLDeque.java
+++ DLLDeque.java
@@ -25,17 +25,16 @@ public class DLLDeque<T> implements Deque<T>{
     //adds new value to the front
     public void addFirst (T val) {
 	//make sure the data structure isn't empty
-	if (isEmpty()) {
-	    _first = new DLLNode<T>(val, null, null);
-	    _last = _first;
-	    _size++;
+	if (isEmpty()) { // if making first node
+	    _first = new DLLNode<T>(val, null, null); //makes the first node a new DLLNode with cargo = val, and no next or prev node
+	    _last = _first; // since this is the only node, it is the first and also the last
+	    _size++;// size increased by one, no longer empty
 	    return;
 	}
 	//otherwise, add to front
-	_first.setPrev(new DLLNode<T>(val, null, _first));
-	//shift pointer over
-	_first = _first.getPrev();
-	_size++;
+	_first.setPrev(new DLLNode<T>(val, null, _first)); // new first node with cargo = val, next node = old first node
+	_first = _first.getPrev(); // moves pointer from the old first node to the new first node
+	_size++;// size increased by one
 	return;
     }//O(1)
 
@@ -43,44 +42,41 @@ public class DLLDeque<T> implements Deque<T>{
     //adds new value to the back
     public void addLast(T val) {
 	//make sure the data structure isn't empty
-	if (isEmpty()) {
-	    _last = new DLLNode<T>(val, null, null);
-	    _first = _last;
-	    _size++;
+	if (isEmpty()) {// if making first node
+	    _last = new DLLNode<T>(val, null, null);//makes the last node a new DLLNode with cargo = val, and no next or prev node
+	    _first = _last;// since this is the only node, it is the last and also the first
+	    _size++;// size increased by one
 	    return;
 	}
 	//otherwise, add to back
-	_last.setNext(new DLLNode<T>(val, _last, null));
-	//shift pointer over
-	_last = _last.getNext();
-	_size++;
+	_last.setNext(new DLLNode<T>(val, _last, null));// new last node with cargo = val, prev node = old last node
+	_last = _last.getNext(); // moves pointer from the old last node to the new last node
+	_size++;// size increased by one
 	return;
     }//O(1)
 
     //removes first value from the front
     public T removeFirst() {
 	//make sure the data structure isn't empty
-        if (isEmpty())
+        if (isEmpty()) //if no first to remove
 	    {throw new IndexOutOfBoundsException("There is no element to remove");}
 	//otherwise, store and remove
-	T temp = _first.getCargo();
-	//shift pointer over
-	_first = _first.getNext();
-	_size--;
-	return temp;
+	T temp = _first.getCargo();// store old first val as temp variable
+	_first = _first.getNext(); //move pointer to next node after old first node
+	_size--; // decrease size by one
+	return temp; // return old val of first
     }//O(1)
 
     //removes last value from the back
     public T removeLast() {
 	//make sure the data structure isn't empty
-        if (isEmpty())
+        if (isEmpty())//if no last to remove
 	    {throw new IndexOutOfBoundsException("There is no element to remove");}
 	//otherwise, store and remove
-	T temp = _last.getCargo();
-	//shift pointer over
-	_last = _last.getPrev();
-	_size--;
-	return temp;
+	T temp = _last.getCargo();// store old last val as temp variable
+	_last = _last.getPrev(); //move pointer to prev node before old last node
+	_size--;// decrease size by one
+	return temp;// return old val of the last
     }//O(1)
 
     //returns the first value in the deque
@@ -108,6 +104,7 @@ public class DLLDeque<T> implements Deque<T>{
 	}
 	return foo;
     }//O(n)
+    
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     public static void main( String[] args ) 
@@ -148,5 +145,5 @@ public class DLLDeque<T> implements Deque<T>{
 	System.out.println("\nnow dequeuing fr empty queue..."); 
 	System.out.println( PirateDeque.removeFirst() );
 	System.out.println( PirateDeque.removeLast() );
-    }
-}//end
+    }//end main method
+}//end DLLDeque
